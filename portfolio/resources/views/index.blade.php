a3840108cc44d4aff61a6ba03c1520f1da3be422
cyxSetyo <cahyasetya003@gmail.com>
2023-07-31 00:50:06 +0700

    create

---
+++ portfolio/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Portfolio</title>
+</head>
+<body>
+    <!-- #MAIN -->
+    <main>
+        <!-- #SIDEBAR -->
+        <aside class="sidebar" data-sidebar>
+            <div class="sidebar-info">
+
+            </div>
+        </aside> 
+
+    <!-- #MAIN CONTENT -->
+        <div class="main-content">
+            
+        </div>
+    </main>
+
+      <!--
+    - custom js link
+  -->
+  <script src="js/script.js"></script>
+</body>
+</html>
